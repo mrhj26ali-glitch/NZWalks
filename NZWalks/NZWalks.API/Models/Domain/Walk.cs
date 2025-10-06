92773feac6b5253a981a575da37be99fbc28bc7d
Ali Mrhj <mrhj26ali@gmail.com>
2025-10-07 02:48:10 +0300

    on the 16th video

---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public  double length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDiffId { get; set; }
+        
+        //Navigation Property
+        public Region Region { get; set; }  
+        public WalkDifficulty WalkDifficulty { get; set; }
+
+
+    }
+}
